d2af36151c820c6880d900e92b19e980118940ec
Brian Vu <emailbrian.vu@gmail.com>
2022-10-04 13:39:46 -0700

    feat: check for jq and install

--- src/scripts/check-health.sh
+++ src/scripts/check-health.sh
@@ -1,11 +1,24 @@
-#!/bin/sh
+if grep "Alpine" < /etc/issue >/dev/null 2>&1; then
+    if [ "$ID" = 0 ]; then export SUDO=""; else export SUDO="sudo"; fi
+    if [ "$(jq --version > /dev/null; echo $?)" -ne 0 ]; then
+        $SUDO apk update
+        $SUDO apk add jq
+    fi
+ else
+    if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
+    if [ "$(jq --version > /dev/null; echo $?)" -ne 0 ]; then
+        $SUDO apt update
+        $SUDO apt install jq
+    fi
+fi
+
 PARAM_AWS_HEALTH_REGION_TO_CHECK=$(eval echo "\$$PARAM_AWS_HEALTH_REGION_TO_CHECK")
 
 FILTER="{\"regions\": ["\"${PARAM_AWS_HEALTH_REGION_TO_CHECK}\""],\"eventTypeCategories\": [\"issue\"], \"eventStatusCodes\": [\"open\",\"upcoming\"]}"
 echo "Checking Health for ${PARAM_AWS_HEALTH_REGION_TO_CHECK} region"
 
-i=0
-while [ "$i" -lt "$PARAM_AWS_HEALTH_MAX_POLL_ATTEMPTS" ]
+i=1
+while [ "$i" -le "$PARAM_AWS_HEALTH_MAX_POLL_ATTEMPTS" ]
 do
     echo "Poll Attempt #$i"
     AWS_EVENTS=$(aws health describe-events --filter "${FILTER}" | jq .events)
