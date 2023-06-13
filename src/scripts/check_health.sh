43f9bbe92121ed48cf6a9fa6788727bd8b9c7533
Brian Vu <emailbrian.vu@gmail.com>
2023-06-12 17:19:49 -0700

    test: debug

--- src/scripts/check_health.sh
+++ src/scripts/check_health.sh
@@ -15,6 +15,7 @@ if grep "Alpine" /etc/issue > /dev/null 2>&1; then
     fi
 fi
 
+
 ORB_EVAL_REGION_TO_CHECK="$(circleci env subst "${ORB_EVAL_REGION_TO_CHECK}")"
 ORB_EVAL_PROFILE="$(circleci env subst "${ORB_EVAL_PROFILE}")"
 
